762c30b17d1f4c6cde19a899687f10f6cc528d99
bhaa225 <117650059+bhaa225@users.noreply.github.com>
2024-04-25 19:01:11 +0300

    implemented Course.java + CourseFolderKeys.java + CourseNotes.java

--- project/Course.java
+++ project/Course.java
@@ -1,8 +1,10 @@
 package project;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -19,101 +21,75 @@ public class Course {
     private File courseDirectory;
     private List<String> relatedNames;
     private File[] filesContained;
+    private BufferedReader br;
+    private CourseNotes notes; 
 
-   
-
-    public Course(File courseFolder){
-        courseDirectory= courseFolder;
+    public Course(File courseFolder) {
+        courseDirectory = courseFolder;
         folderName = courseFolder.getName();
         filesContained = courseFolder.listFiles();
-        File relatedNamesFile = new File(courseFolder,CourseFolderKeys.RELATED_NAMES_FILE_NAME);
-        if(relatedNamesFile.exists()){
+        notes = new CourseNotes(courseDirectory);
+        setRelatedNames();
+    }
+
+    private void setRelatedNames() {
+        File relatedNamesFile = new File(courseDirectory, CourseFolderKeys.RELATED_NAMES_FILE_NAME);
+        if (relatedNamesFile.exists()) {
             try (BufferedReader br = new BufferedReader(new FileReader(relatedNamesFile))) {
                 String line;
-                
+
                 while ((line = br.readLine()) != null) {
                     relatedNames.add(line);
                 }
             } catch (IOException e) {
                 e.printStackTrace(); // Handle any IO exceptions
             }
-        }else{
-            throw new IllegalArgumentException("there is no relatedNames.txt file in the -"+folderName+"- folder");
+        } else {
+            throw new IllegalArgumentException("there is no relatedNames.txt file in the -" + folderName + "- folder");
         }
     }
-    
 
     // Methods
     // Add a file to the specified path
     public void addFile(File fileToAdd, String path) {
         // Implementation
         String folderToAddIn = CourseFolderKeys.getRelatedFolder(fileToAdd.getName());
-        try{
+        try {
             Path toAddPath = Paths.get(fileToAdd.getPath());
             String toAddName = fileToAdd.getName();
-            String targetPath = courseDirectory.toPath()+"/"+folderToAddIn+"/";
+            String targetPath = courseDirectory.toPath() + "/" + folderToAddIn + "/";
             Files.copy(toAddPath, new File(targetPath, toAddName).toPath(), StandardCopyOption.REPLACE_EXISTING);
-        }catch(IOException ex){
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
 
-
     // Remove a file with the given name from the specified path
     public void removeFile(String fileToRemove, String path) {
         // Implementation
-        File fileToDel = new File(path+"/"+fileToRemove);
-        if(fileToDel.exists()){
+        File fileToDel = new File(path + "/" + fileToRemove);
+        if (fileToDel.exists()) {
             fileToDel.delete();
-        }else{
+        } else {
             throw new IllegalArgumentException("file to delete is not existed");
         }
     }
 
     // Check if a file with the given name is related
-    public boolean checkRelated(String fileName) {
-        // Implementation
+    public boolean checkRelatedToCourse(String fileName) {
         for (String name : relatedNames) {
-            if(fileName.contains(name)) return true;
+            if (fileName.contains(name))
+                return true;
         }
         return false;
     }
-
-    // // Move the file with the given name to the related folder
-    // public boolean moveToRelatedFolder(String fileName) {
-    //     // Implementation
-    //     return false;
-    // }
-
-    // Get notes
-    public Map<Integer, String> getNotes() {
-        File notesFile = new File(courseDirectory,CourseFolderKeys.NOTES_FILE_NAME);
-        Map<Integer,String> toRet = new HashMap<>();
-        if(notesFile.exists()){
-            try (BufferedReader br = new BufferedReader(new FileReader(notesFile))) {
-                String line;
-                int index=0;
-                while ((line = br.readLine()) != null) {
-                    toRet.put(index,line);
-                    index++;
-                }
-            } catch (IOException e) {
-                e.printStackTrace(); // Handle any IO exceptions
-            }
-        }else{
-            throw new IllegalArgumentException("there is no relatedNames.txt file in the -"+folderName+"- folder");
-        }
-        return toRet;
+    public ArrayList<String> getNotes() {
+        return notes.getSeparetedLines();
     }
-
-    // Add a string line
-    public void addString(String line) {
-        // Implementation
-
-    }
-
-    // Remove a line with the given number
-    public void removeLine(int number) {
-        // Implementation
+    public void addNote(String note) {
+        notes.writeNewLine(note);
+    }    
+    public void removeLine(int numberToRemove) {
+        notes.removeLine(numberToRemove);
     }
 }
---
+++ project/CourseNotes.java
@@ -0,0 +1,73 @@
+package project;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class CourseNotes {
+    private File noteFile;
+    private BufferedReader br;
+    public CourseNotes(File CourseDirectory){
+        File notesFile = new File(CourseDirectory, CourseFolderKeys.NOTES_FILE_NAME);
+        if (notesFile.exists()) {
+            this.noteFile= notesFile;
+        } else {
+            throw new IllegalArgumentException("note file not exist in course : " + CourseDirectory.getName());
+        }
+    }
+
+    public ArrayList<String> getSeparetedLines(){
+        ArrayList< String> toRet = new ArrayList();
+        Runnable toDo = ()->{
+            String line;
+            try {
+                while ((line = br.readLine()) != null) {
+                    toRet.add(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        };
+        readFileAndDo(toDo);
+        return toRet;
+    } 
+    private void readFileAndDo(Runnable toDoWithFile){
+        try {
+            br = new BufferedReader(new FileReader(noteFile));
+            toDoWithFile.run();
+        } catch (IOException e) {
+            e.printStackTrace(); // Handle any IO exceptions
+        }
+
+    }
+    public void writeNewContent(ArrayList<String> newContent){
+        String contentToWrite = mergeLines(newContent);
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(noteFile))){
+            writer.write(contentToWrite);
+        }catch(IOException e){}
+    }
+
+    private String mergeLines(ArrayList<String> lines) {
+        StringBuilder toRet = new StringBuilder();
+        for(String line:lines){
+            toRet.append(line).append("\n");
+        }
+        return toRet.toString();
+    }
+
+    public void writeNewLine(String line){
+        ArrayList<String> content = getSeparetedLines();
+        content.add(line);
+        writeNewContent(content);
+    }
+
+    public void removeLine(int lineNum){
+        ArrayList<String> content = getSeparetedLines();
+        if(lineNum<content.size()) content.remove(lineNum);
+        writeNewContent(content);
+    }
+}
